4653fc18cdece0c7208866434b22ba0f6192c40f
akomathu <pc-504@st-ove.co.jp>
2024-04-01 14:33:48 +0900

    a

--- Desktop/react-todo-app/src/App.js
+++ Desktop/react-todo-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import Todo from './Todo';
+import Todolist from './Todolist';
 
 function App() {
+  
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+
+  }
+
+
+  const toggleTodo = () =>{
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React  hh
-        </a>
-      </header>
+  <div>
+    <div>
+    <h1>ToDoList</h1>
+    <input type="text"/>
+    <button onClick={addTodo}>タスクを追加</button>
     </div>
-  );
+    <div>
+      <ul>
+      </ul>
+    </div>
+  </div>
+  )
+
 }
+
+
 
 export default App;
